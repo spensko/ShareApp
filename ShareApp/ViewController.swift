9d57d66650fccf1821d8b13f51582d8b6a644d95
Mikhail Serov <iteron@gmail.com>
2019-11-23 14:24:09 +0300

    Initial Commit

---
+++ ShareApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareApp
+//
+//  Created by Mikhail Serov on 23.11.2019.
+//  Copyright © 2019 serov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
